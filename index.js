f077a6ea9b365e599eb4600d36f07eaa8226293c
Christopher Iza <cdiza5@espe.edu.ec>
2024-06-21 11:38:41 -0500

    docker y url

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(8800, () => {
   console.log("Connected in port 8800");
 });
